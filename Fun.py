07d3ad8001ecc45e7677858f282a1adc749dc4f5
deepak-dk <svidhyut31@gmail.com>
2020-12-06 14:14:48 +0530

    merge

--- Fun.py
+++ Fun.py
@@ -1,7 +1,7 @@
 name=input("enter you name")
 str(name)
 age=input("enter you age")
-if(name!=null)
+if(name!=null and age>=18 or age<=25)
       printnl("welcome"+name+" "+age);
  else
       println("Bad request" " please try again");
