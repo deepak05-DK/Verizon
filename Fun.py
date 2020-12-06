177b2580a210a13efa3cccf527b72e87a40b844b
deepak-dk <70804733+deepak05-DK@users.noreply.github.com>
2020-12-06 13:56:25 +0530

    Update Fun.py

--- Fun.py
+++ Fun.py
@@ -1,7 +1,7 @@
 name=input("enter you name")
 str(name)
 age=input("enter you age")
-if(name!=null and age>=18)
+if(name!=null and age>=18 or <=25)
       printnl("welcome"+name+" "+age);
  else
       println("Bad request,");
