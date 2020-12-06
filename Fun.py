8e5c9ca1df6ca098353f43598777ffc11a866d20
deepak-dk <svidhyut31@gmail.com>
2020-12-06 13:01:47 +0530

    new feature

--- Fun.py
+++ Fun.py
@@ -2,6 +2,6 @@ name=input("enter you name")
 str(name)
 age=input("enter you age")
 if(name!=null and age>=18)
-      printnl("welcome"+name);
+      printnl("welcome"+name+" "+age);
  else
       println("Bad request,Please try again");
