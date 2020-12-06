bf30523d8d3d5c928603c47f9f27db7ef0309787
deepak-dk <70804733+deepak05-DK@users.noreply.github.com>
2020-12-06 14:11:40 +0530

    age validation removed

--- Fun.py
+++ Fun.py
@@ -1,7 +1,7 @@
 name=input("enter you name")
 str(name)
 age=input("enter you age")
-if(name!=null and age>=18 or age<=25)
+if(name!=null)
       printnl("welcome"+name+" "+age);
  else
       println("Bad request" " please try again");
